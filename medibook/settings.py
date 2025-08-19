679160347a7942a9d227629e1f0656c24b7abef4
Bhuvana <gbhuvana938@gmail.com>
2025-08-19 14:32:19 +0530

    changed database to mysql

--- medibook/settings.py
+++ medibook/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = "medibook.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'medibook_db',      # Your database name
+        'USER': 'root',             # Your MySQL username
+        'PASSWORD': 'Bhuvana@123',# Your MySQL password
+        'HOST': '127.0.0.1',        # Or 'localhost'
+        'PORT': '3306',             # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
